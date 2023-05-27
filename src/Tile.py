d50af7323d7399b0beb9be10c1b6a087f9cd8b56
JakubTuta <jakubtuta@o2.pl>
2023-05-27 23:14:41 +0200

    Poprawa klasy Tile

--- src/Tile.py
+++ src/Tile.py
@@ -1,28 +1,61 @@
+import pygame
+import os
+
 BOARD_SIZE = 8
 WINDOW_SIZE = 800
 TILE_SIZE = WINDOW_SIZE / BOARD_SIZE
 
 class Tile:
     id = 0
-    def __init__(self, xPos = 0, yPos = 0, wall = "bottom", freeTileGraphic = None, boughtTileGraphic = None):
-        self.__xPos = xPos
-        self.__yPos = yPos
-        self.__tileWidth = 0
-        self.__tileHeight = 0
-        self.wall = wall
-        
-        if wall == "corner":
-            self.__tileWidth = 2 * TILE_SIZE
-            self.__tileHeight = 2 * TILE_SIZE
-        if wall == "bottom" or wall == "top":
-            self.__tileWidth = TILE_SIZE
-            self.__tileHeight = 2 * TILE_SIZE
-        elif wall == "left" or wall == "right":
-            self.__tileWidth = 2 * TILE_SIZE
-            self.__tileHeight = TILE_SIZE
+
+    def __init__(self, WIN, xPos, yPos, name):
+        self.xPos = xPos
+        self.yPos = yPos
+        self.name = name
+        self.WIN = WIN
         
         self.id = Tile.id
         Tile.id += 1
-    
-    def getPos(self):
-        return (self.__x, self.__y)
+
+
+class Property(Tile):
+    def __init__(self, WIN, xPos, yPos, name, wall, notBoughtGraphic, boughtGraphic):
+        super().__init__(WIN, xPos, yPos, name)
+        self.__tileWidth = 0
+        self.__tileHeight = 0
+        self.__notBoughtGraphic = None
+        self.__boughtGraphic = None
+        self.isBought = True
+
+        if wall == "bottom" or wall == "top":
+            self.__tileWidth = TILE_SIZE
+            self.__tileHeight = 2 * TILE_SIZE
+            self.__notBoughtGraphic = pygame.Surface.convert(pygame.transform.scale(pygame.image.load(os.path.join(f"assets/{notBoughtGraphic}")), (self.__tileWidth, self.__tileHeight)))
+            self.__boughtGraphic = pygame.Surface.convert(pygame.transform.scale(pygame.image.load(os.path.join(f"assets/{boughtGraphic}")), (self.__tileWidth, self.__tileHeight)))
+        elif wall == "left":
+            self.__tileWidth = 2 * TILE_SIZE
+            self.__tileHeight = TILE_SIZE
+            self.__notBoughtGraphic = pygame.Surface.convert(pygame.transform.rotate(pygame.transform.scale(pygame.image.load(os.path.join(f"assets/{notBoughtGraphic}")), (self.__tileHeight, self.__tileWidth)), 90))
+            self.__boughtGraphic = pygame.Surface.convert(pygame.transform.rotate(pygame.transform.scale(pygame.image.load(os.path.join(f"assets/{boughtGraphic}")), (self.__tileHeight, self.__tileWidth)), 90))
+        elif wall == "right":
+            self.__tileWidth = 2 * TILE_SIZE
+            self.__tileHeight = TILE_SIZE
+            self.__notBoughtGraphic = pygame.Surface.convert(pygame.transform.rotate(pygame.transform.scale(pygame.image.load(os.path.join(f"assets/{notBoughtGraphic}")), (self.__tileHeight, self.__tileWidth)), 270))
+            self.__boughtGraphic = pygame.Surface.convert(pygame.transform.rotate(pygame.transform.scale(pygame.image.load(os.path.join(f"assets/{boughtGraphic}")), (self.__tileHeight, self.__tileWidth)), 270))
+
+    def draw(self):
+        if not self.isBought:
+            self.WIN.blit(self.__notBoughtGraphic, (self.xPos * self.__tileWidth, self.yPos * self.__tileHeight))
+        else:
+            self.WIN.blit(self.__boughtGraphic, (self.xPos * self.__tileWidth, self.yPos * self.__tileHeight))
+
+
+class Special(Tile):
+    def __init__(self, WIN, xPos, yPos, name, graphic):
+        super().__init__(WIN, xPos, yPos, name)
+        self.__tileWidth = 2 * TILE_SIZE
+        self.__tileHeight = 2 * TILE_SIZE
+        self.__graphic = pygame.Surface.convert(pygame.transform.scale(pygame.image.load(os.path.join(f"assets/{graphic}")), (self.__tileWidth, self.__tileHeight)))
+
+    def draw(self):
+        self.WIN.blit(self.__graphic, (self.xPos * self.__tileWidth, self.yPos * self.__tileHeight))
