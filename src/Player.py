1aec5dee33193126b6e3132485abd9b18da5b857
dziabolud <97457768+dziabolud@users.noreply.github.com>
2023-06-10 10:46:43 +0200

    Dodanie możliwości poruszania się o wyrzuconą ilość na kostce

--- src/Player.py
+++ src/Player.py
@@ -1,22 +1,12 @@
-import pygame
-import os
-
-from Tile import BOARD_SIZE, TILE_SIZE, WINDOW_SIZE
-
 class Player:
     def __init__(self, xPos = 0, yPos = 0, tokenColor = "white", moneyAvailable = 1500):
+        self._currentTile = 0
         self.__xPos = xPos
         self.__yPos = yPos
         self.__tokenColor = tokenColor
         self.__moneyAvailable = moneyAvailable
-        self.image = pygame.image.load(os.path.join("assets", f"Type={self.__tokenColor}.png"))  # Ścieżka do pliku graficznego gracza
-        
-        # Ograniczenia ruchu na obrzeżach planszy
-        self.minX = 0
-        self.maxX = BOARD_SIZE - 1
-        self.minY = 0
-        self.maxY = BOARD_SIZE - 1
-
+        self.hasRolled = False
+    
     def getPos(self):
         return (self.__xPos, self.__yPos)
     
@@ -25,25 +15,9 @@ class Player:
     
     def getMoneyAvailable(self):
         return self.__moneyAvailable
-
-    def move(self, direction, board):
-        if direction == "up":
-            if self.__yPos > self.minY and (self.__xPos == self.maxX or self.__xPos == self.minX ):
-                self.__yPos -= 1
-        elif direction == "down":
-            if self.__yPos < self.maxY and (self.__xPos == self.maxX or self.__xPos == self.minX ):
-                self.__yPos += 1
-        elif direction == "left":
-            if self.__xPos > self.minX and (self.__yPos == self.maxY or self.__yPos == self.minY ):
-                self.__xPos -= 1
-        elif direction == "right":
-            if self.__xPos < self.maxX and (self.__yPos == self.maxY or self.__yPos == self.minY ):
-                self.__xPos += 1
     
-    def draw(self, WIN):
-        player_width, player_height = self.image.get_size()
-        player_pos = (
-            int(self.__xPos * TILE_SIZE + (TILE_SIZE - player_width) / 2),
-            int(self.__yPos * TILE_SIZE + (TILE_SIZE - player_height) / 2),
-        )
-        WIN.blit(self.image, player_pos)
+    def getCurrentTile(self):
+        return self._currentTile
+
+    def setCurrentTile(self, newTile):
+        self._currentTile = newTile
--- src/mainGame.py
+++ src/mainGame.py
@@ -1,123 +1,220 @@
-from random import shuffle
+from random import shuffle, randint
 from Player import Player
-from Tile import BOARD_SIZE, TILE_SIZE, Tile, Property, Special, WINDOW_SIZE
-import pygame
+from Tile import BOARD_SIZE, Tile, Property, Special, WINDOW_SIZE, TILE_SIZE
+import pygame                                                   # pip install pygame / pip install pygame --pre
 import os
+PLAYER_IMAGES = {
+    "red": pygame.image.load(os.path.join("assets", "Type=red.png")),
+    "green": pygame.image.load(os.path.join("assets", "Type=green.png")),
+    "blue": pygame.image.load(os.path.join("assets", "Type=blue.png")),
+    "yellow": pygame.image.load(os.path.join("assets", "Type=yellow.png"))
+}
+
+DICE_SIZE = 60
+DICE_POS = WINDOW_SIZE / 2 - DICE_SIZE / 2
+
+BUTTON_WIDTH = 100
+BUTTON_HEIGHT = 45
+
+ROLL_DICE_BUTTON_POSX = WINDOW_SIZE / 2 - BUTTON_WIDTH / 2
+ROLL_DICE_BUTTON_POSY = WINDOW_SIZE / 2 - DICE_SIZE / 2 + DICE_SIZE + 10
+
+NEXT_TURN_BUTTON_POSX = ROLL_DICE_BUTTON_POSX
+NEXT_TURN_BUTTON_POSY = ROLL_DICE_BUTTON_POSY + BUTTON_HEIGHT + 5
+
+COLORS = {
+    "BLACK": (0, 0, 0),
+    "WHITE": (255, 255, 255),
+    "red": (255, 0, 0),
+    "green": (0, 255, 0),
+    "blue": (0, 0, 255),
+    "yellow": (255, 214, 0)
+}
+
 
 def createPlayersArray(amountOfPlayers):
     if amountOfPlayers < 2 or amountOfPlayers > 4:
         return
     
-    colors = ["Red", "Green", "Blue", "Yellow"]
+    colors = ["red", "green", "blue", "yellow"]
     shuffle(colors)
-    return [Player(7, 7, colors[i]) for i in range(amountOfPlayers)]
+    players = {}
+    for color in colors:
+        players[color] = Player(7, 7, color)
+        players[color].setCurrentTile(1)
+    
+    return players, colors
 
 
 def createBoard(WIN):
     boardArr = []
     
     # bottom wall
-    boardArr.append(Special(WIN, 7, 7, "Start" ,"Type=Start.png"))
-    boardArr.append(Property(WIN, 6, 7, "Lodz", "bottom", "State=Empty, Type=Lodz.png", "State=Bought, Type=Lodz.png"))
-    boardArr.append(Property(WIN, 5, 7, "Cracow", "bottom", "State=Empty, Type=Cracow.png", "State=Bought, Type=Cracow.png"))
-    boardArr.append(Property(WIN, 4, 7, "Warsaw", "bottom", "State=Empty, Type=Warsaw.png", "State=Bought, Type=Warsaw.png"))
-    boardArr.append(Property(WIN, 3, 7, "Berlin", "bottom", "State=Empty, Type=Berlin.png", "State=Bought, Type=Berlin.png"))
-    boardArr.append(Property(WIN, 2, 7, "Munich", "bottom", "State=Empty, Type=Munich.png", "State=Bought, Type=Munich.png"))
-    boardArr.append(Property(WIN, 1, 7, "Dortmund", "bottom", "State=Empty, Type=Dortmund.png", "State=Bought, Type=Dortmund.png"))
+    boardArr.append(Special(WIN, 7, 7, "Start" ,"Type=Start.png", 1))
+    boardArr.append(Property(WIN, 6, 7, "Lodz", "bottom", "State=Empty, Type=Lodz.png", "State=Bought, Type=Lodz.png", 2))
+    boardArr.append(Property(WIN, 5, 7, "Cracow", "bottom", "State=Empty, Type=Cracow.png", "State=Bought, Type=Cracow.png", 3))
+    boardArr.append(Property(WIN, 4, 7, "Warsaw", "bottom", "State=Empty, Type=Warsaw.png", "State=Bought, Type=Warsaw.png", 4))
+    boardArr.append(Property(WIN, 3, 7, "Berlin", "bottom", "State=Empty, Type=Berlin.png", "State=Bought, Type=Berlin.png", 5))
+    boardArr.append(Property(WIN, 2, 7, "Munich", "bottom", "State=Empty, Type=Munich.png", "State=Bought, Type=Munich.png", 6))
+    boardArr.append(Property(WIN, 1, 7, "Dortmund", "bottom", "State=Empty, Type=Dortmund.png", "State=Bought, Type=Dortmund.png", 7))
     
     # left wall
-    boardArr.append(Special(WIN, 0, 7, "Jail" ,"Type=Jail.png"))
-    boardArr.append(Property(WIN, 0, 6, "Lyon", "left", "State=Empty, Type=Lyon.png", "State=Bought, Type=Lyon.png"))
-    boardArr.append(Property(WIN, 0, 5, "Marseille", "left", "State=Empty, Type=Marseille.png", "State=Bought, Type=Marseille.png"))
-    boardArr.append(Property(WIN, 0, 4, "Paris", "left", "State=Empty, Type=Paris.png", "State=Bought, Type=Paris.png"))
-    boardArr.append(Property(WIN, 0, 3, "Liverpool", "left", "State=Empty, Type=Liverpool.png", "State=Bought, Type=Liverpool.png"))
-    boardArr.append(Property(WIN, 0, 2, "Manchester", "left", "State=Empty, Type=Manchester.png", "State=Bought, Type=Manchester.png"))
-    boardArr.append(Property(WIN, 0, 1, "London", "left", "State=Empty, Type=London.png", "State=Bought, Type=London.png"))
+    boardArr.append(Special(WIN, 0, 7, "Jail" ,"Type=Jail.png", 8))
+    boardArr.append(Property(WIN, 0, 6, "Lyon", "left", "State=Empty, Type=Lyon.png", "State=Bought, Type=Lyon.png", 9))
+    boardArr.append(Property(WIN, 0, 5, "Marseille", "left", "State=Empty, Type=Marseille.png", "State=Bought, Type=Marseille.png", 10))
+    boardArr.append(Property(WIN, 0, 4, "Paris", "left", "State=Empty, Type=Paris.png", "State=Bought, Type=Paris.png", 11))
+    boardArr.append(Property(WIN, 0, 3, "Liverpool", "left", "State=Empty, Type=Liverpool.png", "State=Bought, Type=Liverpool.png", 12))
+    boardArr.append(Property(WIN, 0, 2, "Manchester", "left", "State=Empty, Type=Manchester.png", "State=Bought, Type=Manchester.png", 13))
+    boardArr.append(Property(WIN, 0, 1, "London", "left", "State=Empty, Type=London.png", "State=Bought, Type=London.png", 14))
     
     # top wall
-    boardArr.append(Special(WIN, 0, 0, "Parking" ,"Type=Parking.png"))
-    boardArr.append(Property(WIN, 1, 0, "Mediolan", "top", "State=Empty, Type=Mediolan.png", "State=Bought, Type=Mediolan.png"))
-    boardArr.append(Property(WIN, 2, 0, "Venice", "top", "State=Empty, Type=Venice.png", "State=Bought, Type=Venice.png"))
-    boardArr.append(Property(WIN, 3, 0, "Rome", "top", "State=Empty, Type=Rome.png", "State=Bought, Type=Rome.png"))
-    boardArr.append(Property(WIN, 4, 0, "Lisbon", "top", "State=Empty, Type=Lisbon.png", "State=Bought, Type=Lisbon.png"))
-    boardArr.append(Property(WIN, 5, 0, "Porto", "top", "State=Empty, Type=Porto.png", "State=Bought, Type=Porto.png"))
-    boardArr.append(Property(WIN, 6, 0, "Braga", "top", "State=Empty, Type=Braga.png", "State=Bought, Type=Braga.png"))
+    boardArr.append(Special(WIN, 0, 0, "Parking" ,"Type=Parking.png", 15))
+    boardArr.append(Property(WIN, 1, 0, "Mediolan", "top", "State=Empty, Type=Mediolan.png", "State=Bought, Type=Mediolan.png", 16))
+    boardArr.append(Property(WIN, 2, 0, "Venice", "top", "State=Empty, Type=Venice.png", "State=Bought, Type=Venice.png", 17))
+    boardArr.append(Property(WIN, 3, 0, "Rome", "top", "State=Empty, Type=Rome.png", "State=Bought, Type=Rome.png", 18))
+    boardArr.append(Property(WIN, 4, 0, "Lisbon", "top", "State=Empty, Type=Lisbon.png", "State=Bought, Type=Lisbon.png", 19))
+    boardArr.append(Property(WIN, 5, 0, "Porto", "top", "State=Empty, Type=Porto.png", "State=Bought, Type=Porto.png", 20))
+    boardArr.append(Property(WIN, 6, 0, "Braga", "top", "State=Empty, Type=Braga.png", "State=Bought, Type=Braga.png", 21))
 
     # right wall
-    boardArr.append(Special(WIN, 7, 0, "GoToJail" ,"Type=Gotojail.png"))
-    boardArr.append(Property(WIN, 7, 1, "Helsinki", "right", "State=Empty, Type=Helsinki.png", "State=Bought, Type=Helsinki.png"))
-    boardArr.append(Property(WIN, 7, 2, "Stockholm", "right", "State=Empty, Type=Stockholm.png", "State=Bought, Type=Stockholm.png"))
-    boardArr.append(Property(WIN, 7, 3, "Oslo", "right", "State=Empty, Type=Oslo.png", "State=Bought, Type=Oslo.png"))
-    boardArr.append(Property(WIN, 7, 4, "Sevilla", "right", "State=Empty, Type=Sevilla.png", "State=Bought, Type=Sevilla.png"))
-    boardArr.append(Property(WIN, 7, 5, "Madrid", "right", "State=Empty, Type=Madrid.png", "State=Bought, Type=Madrid.png"))
-    boardArr.append(Property(WIN, 7, 6, "Barcelona", "right", "State=Empty, Type=Barcelona.png", "State=Bought, Type=Barcelona.png"))
+    boardArr.append(Special(WIN, 7, 0, "GoToJail" ,"Type=Gotojail.png", 22))
+    boardArr.append(Property(WIN, 7, 1, "Helsinki", "right", "State=Empty, Type=Helsinki.png", "State=Bought, Type=Helsinki.png", 23))
+    boardArr.append(Property(WIN, 7, 2, "Stockholm", "right", "State=Empty, Type=Stockholm.png", "State=Bought, Type=Stockholm.png", 24))
+    boardArr.append(Property(WIN, 7, 3, "Oslo", "right", "State=Empty, Type=Oslo.png", "State=Bought, Type=Oslo.png", 25))
+    boardArr.append(Property(WIN, 7, 4, "Sevilla", "right", "State=Empty, Type=Sevilla.png", "State=Bought, Type=Sevilla.png", 26))
+    boardArr.append(Property(WIN, 7, 5, "Madrid", "right", "State=Empty, Type=Madrid.png", "State=Bought, Type=Madrid.png", 27))
+    boardArr.append(Property(WIN, 7, 6, "Barcelona", "right", "State=Empty, Type=Barcelona.png", "State=Bought, Type=Barcelona.png", 28))
     
     return boardArr
 
 
-def draw(WIN, board, players):
+def tossDice():
+    moves = randint(1, 6)
+    
+    return moves
+
+
+def handleMouseClick(mousePos):
+    mouseX, mouseY = mousePos
+
+    if ROLL_DICE_BUTTON_POSX <= mouseX <= ROLL_DICE_BUTTON_POSX + BUTTON_WIDTH and ROLL_DICE_BUTTON_POSY <= mouseY <= ROLL_DICE_BUTTON_POSY + BUTTON_HEIGHT:
+        return 1
+    elif NEXT_TURN_BUTTON_POSX <= mouseX <= NEXT_TURN_BUTTON_POSX + BUTTON_WIDTH and NEXT_TURN_BUTTON_POSY <= mouseY <= NEXT_TURN_BUTTON_POSY + BUTTON_HEIGHT:
+        return 2
+
+    return 0
+
+
+
+
+
+def drawPlayerTextMoney(WIN, players, turn):
+    font = pygame.font.SysFont(None, 30)
+    turnFont = pygame.font.SysFont(None, 40, bold=True)
+    
+    turnText = f"{turn}'s turn"
+    labelTurnText = turnFont.render(turnText, True, COLORS[turn])
+    textWidth, textHeight = turnFont.size(turnText)
+    WIN.blit(labelTurnText, ((WINDOW_SIZE / 2 - textWidth / 2), (DICE_POS - textHeight - 10)))
+    
+    for player in players.values():
+        color = player.getTokenColor()
+        money = player.getMoneyAvailable()
+        
+        playerText = f"Player {color}"
+        playerMoney = f"Money: {money}"
+        
+        labelPlayerText = font.render(playerText, True, COLORS[color])
+        labelPlayerMoney = font.render(playerMoney, True, COLORS["WHITE"])
+        
+        textWidth, textHeight = font.size(playerText)
+        
+        if color == "red":
+            xPos = (WINDOW_SIZE / 2) - (textWidth / 2)
+            yPos = WINDOW_SIZE - TILE_SIZE - 60
+        elif color == "green":
+            xPos = TILE_SIZE + 20
+            yPos = (WINDOW_SIZE / 2) - textHeight
+        elif color == "blue":
+            xPos = (WINDOW_SIZE / 2) - (textWidth / 2)
+            yPos = TILE_SIZE + 20
+        else:
+            xPos = WINDOW_SIZE - TILE_SIZE - 20 - textWidth
+            yPos = yPos = (WINDOW_SIZE / 2) - textHeight
+        
+        WIN.blit(labelPlayerText, (xPos, yPos))
+        WIN.blit(labelPlayerMoney, (xPos, yPos + textHeight + 10))
+
+def drawPlayers(WIN, players, board):
+    for player in players.values():
+        tileId = player.getCurrentTile()  # Pobieramy ID pola na planszy
+        tile = next((tile for tile in board if tile.id == tileId), None)  # Wyszukujemy pole o podanym ID
+        if tile:
+            xPos, yPos = int(tile.xPos * TILE_SIZE + TILE_SIZE / 2), int(tile.yPos * TILE_SIZE + TILE_SIZE / 2)
+            color = player.getTokenColor()
+            player_image = PLAYER_IMAGES[color]
+            player_rect = player_image.get_rect(center=(xPos, yPos))
+            WIN.blit(player_image, player_rect)
+
+
+
+
+def draw(WIN, board, players, diceGraphs, RollDiceButtonGraphic, NextTurnButtonGraphic, moves, turn):
     WIN.fill((0, 0, 0))
 
     for tile in board:
         tile.draw()
-    
-    for player in players:
-        x, y = player.getPos()
-        player.draw(WIN)
-    
-    pygame.display.update()
 
-def movePlayer(player, direction, board):
-    player.move(direction, board)
+    drawPlayers(WIN, players, board)  # Dodanie pionków graczy
+    
+    WIN.blit(diceGraphs[moves], (DICE_POS, DICE_POS))
+    WIN.blit(RollDiceButtonGraphic, (ROLL_DICE_BUTTON_POSX, ROLL_DICE_BUTTON_POSY))
+    WIN.blit(NextTurnButtonGraphic, (NEXT_TURN_BUTTON_POSX, NEXT_TURN_BUTTON_POSY))
+    
+    drawPlayerTextMoney(WIN, players, turn)
+
 
 def main(): 
     pygame.init()
     WIN = pygame.display.set_mode((WINDOW_SIZE, WINDOW_SIZE))
     pygame.display.set_caption("Monopoly")
     
+    diceGraphs = {}
+    for i in range(1, 7):
+        diceGraphs[i] = pygame.Surface.convert_alpha(pygame.transform.scale(pygame.image.load(os.path.join(f"assets/State=Dice_{i}.png")), (DICE_SIZE, DICE_SIZE)))
+    RollDiceButtonGraphic = pygame.Surface.convert_alpha(pygame.transform.scale(pygame.image.load(os.path.join(f"assets/State=Dice_button.png")), (BUTTON_WIDTH, BUTTON_HEIGHT)))
+    NextTurnButtonGraphic = pygame.Surface.convert_alpha(pygame.transform.scale(pygame.image.load(os.path.join(f"assets/Next_turn_button.png")), (BUTTON_WIDTH, BUTTON_HEIGHT)))
+    
     board = createBoard(WIN)
-    playersArr = createPlayersArray(4)
-    currentPlayerIndex = 0
-    is_move_allowed = True  # Zmienna określająca, czy można wykonywać ruchy
-    enter_pressed = False  # Zmienna śledząca, czy klawisz Enter został wciśnięty
+    players, turns = createPlayersArray(4)
+    currentTurn = turns.pop(0)
+    turns.append(currentTurn)
     
     clock = pygame.time.Clock()
 
-    gameRunning = True
-    while gameRunning:
-        for event in pygame.event.get():
+    moves = 1
+    while True:
+            event = pygame.event.wait()
             if event.type == pygame.QUIT:
-                pygame.quit()
-            elif event.type == pygame.KEYDOWN:
-                if is_move_allowed:
-                    if event.key == pygame.K_UP:
-                        movePlayer(playersArr[currentPlayerIndex], "up", board)
-                    elif event.key == pygame.K_DOWN:
-                        movePlayer(playersArr[currentPlayerIndex], "down", board)
-                    elif event.key == pygame.K_LEFT:
-                        movePlayer(playersArr[currentPlayerIndex], "left", board)
-                    elif event.key == pygame.K_RIGHT:
-                        movePlayer(playersArr[currentPlayerIndex], "right", board)    
-                if event.key == pygame.K_RETURN:  # Naciśnięcie klawisza Enter
-                    enter_pressed = True           
+                break
 
-        draw(WIN, board, playersArr)
-        
-        if enter_pressed:
-            is_move_allowed = False  # Zablokowanie ruchu
-            enter_pressed = False
-
-        pygame.display.update()
-        clock.tick(60)
-
-        if not is_move_allowed:
-            if currentPlayerIndex < len(playersArr) - 1:
-                currentPlayerIndex += 1
-            else:
-                currentPlayerIndex = 0
-            is_move_allowed = True  # Odblokowanie ruchu dla nowego gracza
-        
-    pygame.quit()    
+            draw(WIN, board, players, diceGraphs, RollDiceButtonGraphic, NextTurnButtonGraphic, moves, currentTurn)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                action = handleMouseClick(pygame.mouse.get_pos())
+                if action == 1 and not players[currentTurn].hasRolled: # roll the dice button clicked
+                    players[currentTurn].hasRolled = True
+                    moves = tossDice()
+                    currentPlayer = players[currentTurn]
+                    currentTile = currentPlayer.getCurrentTile()
+                    newTile = (currentTile + moves) % len(board)
+                    currentPlayer.setCurrentTile(newTile)
+                elif action == 2: # next turn button clicked
+                    players[currentTurn].hasRolled = False
+                    currentTurn = turns.pop(0)
+                    turns.append(currentTurn)
+            
+            pygame.display.update()
+            clock.tick(60)
 
 if __name__ == "__main__":
     main()
